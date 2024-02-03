a1b472fe495a967e731416d971e1fbb7b550de2a
Shakil Ahmed <ahmedshakil00071@gmail.com>
2024-02-03 18:25:41 +0600

    complete the task two

--- function.js
+++ function.js
@@ -1,8 +1,22 @@
 // task one
 
-function multiply (num1, num2, num3, num4){
-    const result = num1 * num2 * num3 * num4;
-    console.log('total multiply result is =', result);
+// function multiply (num1, num2, num3, num4){
+//     const result = num1 * num2 * num3 * num4;
+//     console.log('total multiply result is =', result);
+//     return result;
+// }
+// multiply(5, 10, 15, 20);
+
+
+
+//task two
+function oddEven (number){
+    let result = '';
+    if(number % 2 === 1){
+        console.log( number * 2);
+    } else{ 
+        console.log(number / 2);
+    }
     return result;
 }
-multiply(5, 10, 15, 20);
+oddEven(12);
