177a6e4fc93666b50328e429693000b25aa93e92
Shakil Ahmed <ahmedshakil00071@gmail.com>
2024-02-03 18:10:21 +0600

    tast one complete

---
+++ function.js
@@ -0,0 +1,8 @@
+// task one
+
+function multiply (num1, num2, num3, num4){
+    const result = num1 * num2 * num3 * num4;
+    console.log('total multiply result is =', result);
+    return result;
+}
+multiply(5, 10, 15, 20);
